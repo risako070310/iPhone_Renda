bf4f60263f4f65e1d864c55d6a676c7765ae268e
risako070310 <risako070310@gmail.com>
2022-06-04 02:30:53 +0900

    create renda app

--- Renda/ViewController.swift
+++ Renda/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    @IBOutlet var tapButton: UIButton!
+    
+    var tapCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tapButton.clipsToBounds = true
+        tapButton.layer.cornerRadius = 125
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapTapButton(){
+        tapCount = tapCount + 1
+        
+        countLabel.text = String(tapCount)
+    }
 }
 
